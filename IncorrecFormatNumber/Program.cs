17e973fd070ba4a3975210aaeb77ab3a5ff9d541
Andrii Yakovenko <andrii.studying@gmail.com>
2025-06-15 23:14:31 +0300

    Verification of number input from the console

---
+++ IncorrecFormatNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишіть програму, яка зчитує число з рядка. Якщо введене значення не є числом, виведіть повідомлення про помилку.
+*/
+
+Console.WriteLine("Verification of number input from the console");
+Console.WriteLine("");
+
+Console.WriteLine("Enter a number");
+string inputValue = Console.ReadLine();
+Console.WriteLine("");
+try
+{
+    int inputNumber = int.Parse(inputValue);
+    Console.WriteLine($"Result: {inputNumber}");
+    Console.WriteLine("");
+}
+catch (System.Exception)
+{
+    Console.WriteLine($"The entered value is not a number.");
+}
+finally
+{
+    Console.WriteLine("Program ended");
+}
+
+
+
+
