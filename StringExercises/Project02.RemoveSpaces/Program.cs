b606b7bd928e11a9a043c19c5d870bd28b994dc7
Andrii Yakovenko <andrii.studying@gmail.com>
2025-07-03 00:50:15 +0300

    add Project02.RemoveSpaces

---
+++ StringExercises/Project02.RemoveSpaces/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Опис: Ви отримали рядок, який містить декілька слів, розділених комами.
+ Ваша мета - видалити всі пробіли з цього рядка та вивести результат.
+*/
+
+using System.IO.Pipelines;
+Console.Clear();
+var stringName = @"Опис: Ви отримали рядок, який містить декілька слів, розділених комами. Ваша мета - видалити всі пробіли з цього рядка та вивести результат.";
+var rusult = stringName.Replace(" ", "");
+
+Console.WriteLine("Вивід строки без редагування:");
+Console.WriteLine();
+
+Console.Write($"{stringName}");
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("Вивід строки без пробілів:");
+Console.WriteLine();
+
+Console.Write($"{rusult}");
