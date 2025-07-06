e7c52205171b3d09663945bc1f2ff0e5c6c68c06
Andrii Yakovenko <andrii.studying@gmail.com>
2025-07-06 22:46:19 +0300

    add Project03.TextReport

---
+++ StringExercises/Project03.TextReport/Program.cs
@@ -0,0 +1,30 @@
+﻿// Ви розробляєте програму для генерації текстового звіту.
+//  Ваше завдання - створити текстовий звіт, який містить заголовок, дату та список подій.
+//   Використайте StringBuilder, щоб зберегти пам’ять та оптимізувати об’єднання рядків. 
+//   Користувач у циклi буде вводити подiї, треба додавати їх у загальний список подiй типу StringBuilder .
+
+
+using System.Text;
+Console.Clear();
+
+StringBuilder report = new StringBuilder();
+report.AppendLine("Перелік подій:");
+report.AppendLine();
+
+while (true)
+{
+    Console.WriteLine("Для формавання звіту необхідно додати подію (для завершення натисніть Enter):");
+    string inputAction = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(inputAction))
+    {
+        break;
+    }
+
+    report.AppendLine("\t - " + inputAction);
+}
+report.AppendLine();
+report.AppendLine("Дата формування звіту: " + DateTime.Now.ToString("yyyy.MM.dd"));
+
+Console.WriteLine("\t \t \t \t  Звіт");
+Console.WriteLine(report.ToString());
