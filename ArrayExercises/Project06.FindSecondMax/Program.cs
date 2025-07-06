721ce34c0a02280372ae60d91024445c6a4f1664
Andrii Yakovenko <andrii.studying@gmail.com>
2025-07-06 15:37:51 +0300

    add Project06.FindSecondMax

--- ArrayExercises/Project06.FindSecondMax/Program.cs
+++ ArrayExercises/Project06.FindSecondMax/Program.cs
@@ -1,10 +1,28 @@
 ﻿/*
 Написати програму, що знаходить другий найбільший елемент масиву.
 */
-/*
-int[] arrayInt = new int[7];
-
-Random random = new Random;
-
-*/
 Console.Clear();
+
+
+int[] arrayInt = {4, 35, 8, 7, 9, 12,12,25};
+
+int maxValue = int.MinValue;
+int secondMaxValue = int.MinValue;
+
+for (int i = 0; i < arrayInt.Length; i++)
+{
+    if (arrayInt[i] > maxValue)
+    {
+        secondMaxValue = maxValue;
+        maxValue = arrayInt[i];
+    }
+    else if (arrayInt[i] > secondMaxValue && arrayInt[i] != maxValue)
+    {
+        secondMaxValue = arrayInt[i];
+    }
+}
+
+
+Console.WriteLine($"Другий найбільший елемент: {secondMaxValue}");
+
+
