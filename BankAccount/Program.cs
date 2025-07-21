e547432c3dfe9f61f8b168c023556985f0db2d47
Andrii Yakovenko <andrii.studying@gmail.com>
2025-07-21 10:58:19 +0300

    change name

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -4,10 +4,10 @@ Console.Clear();
 Console.WriteLine($"The balance on the my accounts: ");
 
 Account MySalaryAccount = new Account("Salary account", 1000.00);
-Account MyDepositAccount = new Account("Deposit account", 1000000.00);
+Account MyDepositAccount = new Account("Deposit account", 100000.00);
 
-MySalaryAccount.Withdrawal(300);
+MySalaryAccount.Withdrawal(250);
 Console.WriteLine( "\n Salary account: " + MySalaryAccount.Balance);
 
-MyDepositAccount.TopUp(200);
+MyDepositAccount.TopUp(230);
 Console.WriteLine($"\n Deposit account: " + MyDepositAccount.Balance);
