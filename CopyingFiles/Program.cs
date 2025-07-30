2daa32a5042f4c2925cc3d72be146b8177207a71
Andrii Yakovenko <andrii.studying@gmail.com>
2025-07-30 23:14:22 +0300

    add CopyingFiles

---
+++ CopyingFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+Console.Clear();
+Console.Write("Введіть шлях до файлу, який потрібно скопіювати: ");
+string sourceFile = Console.ReadLine();
+
+Console.Write("Введіть шлях до нового файлу: ");
+string destinationFile = Console.ReadLine();
+
+if (!File.Exists(sourceFile))
+{
+    Console.WriteLine("Файл не знайдено. Перевірте шлях.");
+    return;
+}
+
+try
+{
+    string data = File.ReadAllText(sourceFile);
+    File.WriteAllText(destinationFile, data);
+    Console.WriteLine("Копіювання завершено успішно.");
+}
+catch (Exception e)
+{
+    Console.WriteLine("Сталася помилка при копіюванні:");
+    Console.WriteLine(e.Message);
+}
